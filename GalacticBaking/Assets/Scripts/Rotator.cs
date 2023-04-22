ce833cac1354474250898afcb2c90cf0aee00b1e
Kacey Walsh <kaceywalsh@knights.ucf.edu>
2023-04-22 16:58:27 -0400

    Revert "HubRefresh"
    This reverts commit 647beb5548b4f65343092e5f19fb82cba4e129c3.

--- GalacticBaking/Assets/Scripts/Rotator.cs
+++ GalacticBaking/Assets/Scripts/Rotator.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float speed = 100f;
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, speed*5, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
     }
 }

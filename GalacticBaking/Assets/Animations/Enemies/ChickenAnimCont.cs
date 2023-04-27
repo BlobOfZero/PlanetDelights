f7e59486670c9ef86f1b9a1c538bc98cd60a17d3
Kacey Walsh <kaceywalsh@knights.ucf.edu>
2023-04-27 04:25:26 -0400

    AnimationUpdates

---
+++ GalacticBaking/Assets/Animations/Enemies/ChickenAnimCont.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAnimCont : MonoBehaviour
+{
+    Animator animator;
+
+    EnemyAI enemyAI;
+
+    int isChasingHash;
+
+    private void Awake()
+    {
+        //GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyAI.GetComponent<EnemyAI>();
+        //enemyAI = enemy.GetComponent<EnemyAI>();
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isChasingHash = Animator.StringToHash("isChasing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Walking Animation
+        bool isChasing = animator.GetBool(isChasingHash);
+
+
+        Debug.Log(enemyAI.inPursuit);
+
+
+        if (enemyAI.inPursuit == true)
+        {
+        //    Debug.Log();
+            animator.SetBool("isChasing", true);
+        }
+
+        if (enemyAI.inPursuit == false)
+        {
+            animator.SetBool(isChasingHash, false);
+        }
+    }
+}
